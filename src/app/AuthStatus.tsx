04bdaa034d724b98fae875c44547c86850dcfd60
Jiheon Kim <siwmua0@naver.com>
2024-03-27 00:27:32 +0900

    :card_file_box: Mongodb Network acess 수정 및 Signin 페이지 삭제

--- src/app/AuthStatus.tsx
+++ src/app/AuthStatus.tsx
@@ -15,7 +15,21 @@ import LoadingButton from "./components/LoadingButton";
 
 const AuthStatus = () => {
   const { data: session, status } = useSession();
+  const user = session?.user;
   const router = useRouter();
+
+  const handleClick = async () => {
+    router.push("/api/auth/signout");
+  };
+
+  if (status === "loading")
+    return (
+      <Box className="bg-[#d82e5a] p-1 rounded-3xl">
+        <div className="px-2">
+          <LoadingButton />
+        </div>
+      </Box>
+    );
 
   if (status === "unauthenticated")
     return (
@@ -31,49 +45,38 @@ const AuthStatus = () => {
       </Box>
     );
 
-  const handleClick = async () => {
-    router.push("/api/auth/signout");
-  };
-
-  if (status === "loading")
-    return (
-      <Box className="bg-[#d82e5a] p-1 rounded-3xl">
-        <div className="px-2">
-          <LoadingButton />
-        </div>
-      </Box>
-    );
-
   return (
     <Box className="bg-white p-1 rounded-3xl">
-      <Flex align="center" gap="1" className="px-1">
-        <DropdownMenu.Root>
-          <DropdownMenu.Trigger>
-            <Flex align="center" gap="1">
-              <MenuIcon size={20} />
-            </Flex>
-          </DropdownMenu.Trigger>
-          <DropdownMenu.Content>
-            <DropdownMenu.Label>
-              <Text size="2"> {session!.user!.email}</Text>
-            </DropdownMenu.Label>
-            <DropdownMenu.Item color="red" onClick={handleClick}>
-              로그아웃
-            </DropdownMenu.Item>
-          </DropdownMenu.Content>
-        </DropdownMenu.Root>
-        <Avatar
-          src={session!.user!.image!}
-          fallback="?"
-          size="2"
-          radius="full"
-          className="cursor-pointer"
-          referrerPolicy="no-referrer"
-        />
-        <Text size="2" weight="bold">
-          {session!.user!.name}
-        </Text>
-      </Flex>
+      {user && (
+        <Flex align="center" gap="1" className="px-1">
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger>
+              <Flex align="center" gap="1">
+                <MenuIcon size={20} />
+              </Flex>
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content>
+              <DropdownMenu.Label>
+                <Text size="2"> {user.email}</Text>
+              </DropdownMenu.Label>
+              <DropdownMenu.Item color="red" onClick={handleClick}>
+                로그아웃
+              </DropdownMenu.Item>
+            </DropdownMenu.Content>
+          </DropdownMenu.Root>
+          <Avatar
+            src={user.image!}
+            fallback="?"
+            size="2"
+            radius="full"
+            className="cursor-pointer"
+            referrerPolicy="no-referrer"
+          />
+          <Text size="2" weight="bold">
+            {user.name}
+          </Text>
+        </Flex>
+      )}
     </Box>
   );
 };
